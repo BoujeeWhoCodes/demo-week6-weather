99ef32b3e864df29905d90629bd381a1177233be
BoujeeWhoCodes <imellettis28@gmail.com>
2024-05-18 21:22:23 +0800

    Update

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,12 +1,43 @@
-/** @format */
 
 import React from 'react';
+import './WeatherTemperature.css';
 
 export default function WeatherTemperature(props) {
-	return (
-		<div className='WeatherTemperature'>
-			<span className='temperature'>{Math.round(props.celsius)}</span>
-			<span className='unit'>°C</span>
-		</div>
-	);
+	function swapUnits(event) {
+		event.preventDefault();
+		props.change();
+	}
+
+	if (props.unit === 'celsius') {
+		return (
+			<span className='WeatherTemperature'>
+				<span className='temperature'>{Math.round(props.celsius)}</span>
+				<span className='unit'>
+					°C{' '}
+					<a
+						href='/'
+						onClick={swapUnits}
+					>
+						| °F
+					</a>
+				</span>
+			</span>
+		);
+	} else {
+		let fahrenheit = (props.celsius * 9) / 5 + 32;
+		return (
+			<span className='WeatherTemperature'>
+				<span className='temperature'>{Math.round(fahrenheit)}</span>
+				<span className='unit'>
+					°F{' '}
+					<a
+						href='/'
+						onClick={swapUnits}
+					>
+						| °C
+					</a>
+				</span>
+			</span>
+		);
+	}
 }
